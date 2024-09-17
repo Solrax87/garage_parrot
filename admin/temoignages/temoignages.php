51b2f0ae50376ad522bac6e8e31aa70bee9cac3e
Solrax87 <solrac.sr7@gmail.com>
2024-09-17 14:39:06 +0200

    Commit

---
+++ admin/temoignages/temoignages.php
@@ -0,0 +1,107 @@
+<?php 
+
+require '../../includes/functions.php';
+$auth = authentifie();
+$resultat = $_GET['resultat'] ?? null;
+
+//  echo "<pre>";
+//  var_dump($_POST);
+//  echo "</pre>";
+
+    // Importer la connexion
+    require '../../includes/config/database.php';
+    $db = conectBD();
+    // Ecrire le Query
+    $query = "SELECT * FROM temoignages";
+    // Consultation de BD
+    $resultat = mysqli_query($db, $query);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+    
+        if ($id) {
+            $query = "DELETE FROM temoignages WHERE id = {$id}";
+            $result = mysqli_query($db, $query);
+    
+            if ($result) {
+                // Establecer variable de sesión para indicar éxito
+                $_SESSION['temoignage_deleted'] = true;
+                header('Location: temoignages.php');
+                exit;
+            }
+        }
+    }
+
+    if(!$auth) {
+        header('Location: /login.php');
+        exit;
+    }
+    
+
+
+    /** Header */
+    
+    incluTemplates('header');
+?>
+
+<section>
+    <h1>ADMINISTRATEUR DE TÉMOIGNAGES</h1>
+</section>
+
+<div class="btn-admin container">
+
+    <div>
+        <a class="btn-green" href="/admin/propietaire/administrateur.php">Venir en Arrier</a>
+    </div>
+
+</div>
+<section class="container">
+    <?php if (isset($_SESSION['temoignage_deleted'])): ?>
+        <p class="alerte succes">Témoignage Effacé</p>
+        <?php unset($_SESSION['temoignage_deleted']); ?> <!-- Limpiar variable de sesión -->
+    <?php endif; ?>
+    <div>
+        <p class="text-importante">* Supprimer toujours le premier de la liste pour que les nouveaux messages soient reflétés sur la page principale.</p>
+        <p class="text-importante">* 1 message par jour</p>
+    </div>
+
+    <table class="vehicules-all">
+        <thead>
+           <tr>
+            <th>ID</th>
+            <th>NOM</th>
+            <th>COMPAGNIE</th>
+            <th>QUALIFICATION</th>
+            <th>COMMENTAIRE</th>
+            <th>ACTIONS</th>
+           </tr> 
+        </thead>
+
+        <tbody> <!-- Resultat de la BD -->
+        <?php while($temoinages = mysqli_fetch_assoc($resultat)): ?>
+            <tr>
+                <td style="text-align: center;"><?php echo $temoinages['id']; ?></td>
+                <td style="text-align: center;"><?php echo $temoinages['nom']; ?></td>
+                <td style="text-align: center;"><?php echo $temoinages['compagnie']; ?></td>
+                <td style="text-align: center;"><?php echo $temoinages['qualification']; ?></td>
+                <td style="text-align: center;"><?php echo $temoinages['comentaire']; ?></td>
+                <td >
+                     <form method="POST" class="w-100"> <!-- class="w-100" ubication dans _admin.scss -->
+                        <input type="hidden" name="id" value="<?php echo $temoinages['id']; ?> ">
+                        <input type="submit" class="btn-red" value="Effacer">
+                        
+                    </form>
+                </td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+        
+    </table>
+
+</section>
+
+<!-- Footer -->
+<?php 
+    incluTemplates('footer'); 
+?>
---
+++ build/js/app.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+    // Función para manejar el envío del formulario
+    $("#filtre").submit(function(event) {
+        event.preventDefault(); // Evitar la recarga de la página
+
+        // Obtener valores de los controles deslizantes
+        var minPrice = $("#prix").val();
+        var maxPrice = $("#prix-max-value").text();
+        var minKilometrage = $("#kilometrage-min-value").text();
+        var maxKilometrage = $("#kilometrage-max-value").text();
+        var minAnnees = $("#annees-min-value").text();
+        var maxAnnees = $("#annees-max-value").text();
+
+        // Petición AJAX para obtener vehículos filtrados
+        $.ajax({
+            url: 'vehicules_partial.php',
+            method: 'GET',
+            data: {
+                prix_min: minPrice,
+                prix_max: maxPrice,
+                kilometrage_min: minKilometrage,
+                kilometrage_max: maxKilometrage,
+                annees_min: minAnnees,
+                annees_max: maxAnnees
+            },
+            success: function(response) {
+                // Actualizar el contenido de #vehiculesContainer
+                $("#vehiculesContainer").html(response);
+            }
+        });
+    });
+
+    // Restablecer el filtro
+    $("#reset-btn").click(function(event) {
+        event.preventDefault(); // Evitar la recarga de la página
+
+        // Restablecer los valores de los controles deslizantes
+        $("#prix").val(0);
+        $("#prix-min-value").text("0");
+        $("#prix-max-value").text("150000");
+
+        $("#kilometrage").val(0);
+        $("#kilometrage-min-value").text("0");
+        $("#kilometrage-max-value").text("250000");
+
+        $("#annees").val(2001);
+        $("#annees-min-value").text("2001");
+        $("#annees-max-value").text("2025");
+
+        // Volver a enviar el formulario
+        $("#filtre").submit();
+    });
+
+    // Actualizar los valores de los labels de precio cuando cambia el rango de precio
+    $("#prix").on("input", function() {
+        $("#prix-min-value").text($(this).val());
+    });
+
+    // Actualizar los valores de los labels de kilometraje cuando cambia el rango de kilometraje
+    $("#kilometrage").on("input", function() {
+        $("#kilometrage-min-value").text($(this).val());
+    });
+
+    // Actualizar los valores de los labels de años cuando cambia el rango de años
+    $("#annees").on("input", function() {
+        $("#annees-min-value").text($(this).val());
+    });
+});
+
+
+
+    const commentaireElement = document.getElementById("comentaire");
+    if (commentaireElement) {
+        maxLength(commentaireElement);
+    }
+
+    /** Allert buton témoignage */
+    function confirmation() {
+        var reponse = confirm('Souhaitez-vous envoyer les informations ?');
+        return reponse === true;
+    }
+
+    function confirmation2() {
+        var reponse = confirm('Êtes-vous sûr de vouloir supprimer les informations ?');
+        return reponse === true;
+    }
+
+    // Imagenes
+    // Seleccionar la imagen grande
+    const largeImage = document.getElementById('largeImage');
+    
+    // Seleccionar todas las imágenes pequeñas
+    const smallImages = document.querySelectorAll('.smallImage');
+
+    // Añadir un evento de clic a cada imagen pequeña
+    if (largeImage) {
+        smallImages.forEach(function(image) {
+            image.addEventListener('click', function() {
+                // Cambiar el src y el alt de la imagen grande
+                largeImage.src = this.src;
+                largeImage.alt = this.alt;
+            });
+        });
+    }
