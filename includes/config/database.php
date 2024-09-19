ee8ae8bf6f962ec923f51cf652e0b94c6b15a08d
Solrax87 <solrac.sr7@gmail.com>
2024-09-19 16:43:38 +0000

    db load commit

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,12 +1,28 @@
 <?php
 
-function conectBD() : mysqli {
-    $db = mysqli_connect('localhost', 'root', '', 'garage_parrot');
 
-    if(!$db) {
-        echo "Pas connecté";
-        exit;
-    }
+// Configuración de conexión a la base de datos
+$servername = "nue.domcloud.co";  // O "89.58.31.246" si prefieres usar la IP
+$username = "garage-parrot";      // Usuario de la base de datos
+$password = "cl2s3vCRZ)-By16S1-"; // Contraseña de la base de datos 
+$dbname = "garage_parrot";         // Asegúrate de reemplazar esto con el nombre real de tu base de datos
 
-    return $db;
+// Crear la conexión
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if (!$db) {
+    die("Pas connecté: " . mysqli_connect_error());
 }
+
+
+// function conectBD() : mysqli {
+//     $db = mysqli_connect('localhost', 'root', '', 'garage_parrot');
+
+//     if(!$db) {
+//         echo "Pas connecté";
+//         exit;
+//     }
+
+//     return $db;
+// }
