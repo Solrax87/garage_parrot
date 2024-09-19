c794f7036650b1d2f900f33f2307cdca71e3a798
Solrax87 <solrac.sr7@gmail.com>
2024-09-19 21:20:10 +0000

    commit

--- includes/templates/vehicules.php
+++ includes/templates/vehicules.php
@@ -1,63 +1,42 @@
 <?php
-    // Importation de connéxion
-    require __DIR__ . '/../config/database.php'; 
-    //require 'includes/config/database.php'; //Cualquiera de la dos opciones es valida
-    $db = conectBD();
+// Importación de conexión
+require __DIR__ . '/../config/database.php'; 
+$db = conectBD();
 
-    // Consultation
-    $query = "SELECT * FROM vehicules LIMIT {$limite}";
-    // $query_temoignages = "SELECT * FROM temoignages LIMIT 6";
-    
+// Definir límite (si no está definido)
+$limite = isset($limite) ? $limite : 9; 
 
-        // Obtención de parámetros de filtro
-        $minPrice = isset($_GET['prix_min']) ? $_GET['prix_min'] : 0;
-        $maxPrice = isset($_GET['prix_max']) ? $_GET['prix_max'] : 1000000;
+// Obtención de parámetros de filtro de precio
+$minPrice = isset($_GET['prix_min']) && is_numeric($_GET['prix_min']) ? (int)$_GET['prix_min'] : 0;
+$maxPrice = isset($_GET['prix_max']) && is_numeric($_GET['prix_max']) ? (int)$_GET['prix_max'] : 1000000;
 
-         // Si se proporcionan parámetros de filtro, construir la consulta filtrada
-        if ($minPrice !== 0 || $maxPrice !== 1000000) {
-            $query = "SELECT * FROM vehicules WHERE prix >= $minPrice AND prix <= $maxPrice LIMIT 9";
-        }
+// Construir la consulta filtrada
+$query = "SELECT * FROM vehicules WHERE prix >= ? AND prix <= ? LIMIT ?";
+$stmt = mysqli_prepare($db, $query);
+mysqli_stmt_bind_param($stmt, "iii", $minPrice, $maxPrice, $limite);
+mysqli_stmt_execute($stmt);
+$resultat = mysqli_stmt_get_result($stmt);
 
-    // obtention des resultats
-    $resultat = mysqli_query($db, $query);
-    
-
-    /** Espace pour temoignages */
-
-    // Array message erreur
-    
-
-    // echo "<pre>";
-    // var_dump($query);
-    // echo "</pre>";
-
-?>
-
-
-    <div class="all-publicite">
-        
-        <?php while($vehicule = mysqli_fetch_assoc($resultat)): ?>
+// Verificar si hay resultados
+if (mysqli_num_rows($resultat) > 0) {
+    echo '<div class="all-publicite">';
+    while($vehicule = mysqli_fetch_assoc($resultat)): ?>
         <div class="publicite">
-
-                <img loading="lazy" src="/images/<?php echo $vehicule['image1']; ?>" alt="image voiture Mitsubishi">
-
+            <img loading="lazy" src="/images/<?php echo $vehicule['image1']; ?>" alt="image voiture">
             <div class="container-publicite">
                 <h3><?php echo $vehicule['titre']; ?></h3>
                 <h4><?php echo number_format($vehicule['kilometrage'], 0, '', '.'); ?> km</h4>
                 <h4><?php echo 'Mise en Circulation / ' . $vehicule['date_circulation']; ?></h4>
                 <h4><?php echo 'Carburant / ' . $vehicule['energie']?></h4>
                 <p class="prix"><?php echo number_format($vehicule['prix'], 2, ',', '.'); ?> €</p>
-
                 <a href="voiture.php?id=<?php echo $vehicule['id']; ?>" class="btn-yellow">
                     Voir Véhicule
                 </a>
-            </div><!--.container-publicite-->
-        </div><!--.publicite-->
-        <?php endwhile; ?>
-    </div>
-
-    </section>
-    
-    <!-- Section Témoignages -->
-
-
+            </div>
+        </div>
+    <?php endwhile;
+    echo '</div>';
+} else {
+    echo "Aucun véhicule trouvé.";
+}
+?>
